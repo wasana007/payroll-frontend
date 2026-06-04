9f074c8a97af7f487680f1ba51f1fc826997b2db
Wasana <wasana.leithe@gmail.com>
2026-06-04 22:07:31 +0200

    chore: remove unused dependency

--- README.md
+++ README.md
@@ -51,7 +51,7 @@ npm run dev
 
 ## Konfigurasjon
 
-Konfigureres i `src/config.js`:
+Konfigureres i `src/config.ts`:
 
 | Variabel | Standard | Beskrivelse |
 |---|---|---|
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import App from "./App";
-
+ 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
